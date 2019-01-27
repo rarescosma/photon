aa8d7fc507d28b73c7a758974375c6b5d7a10cfc
Rareș Cosma <rares@getbetter.ro>
2019-01-27 16:02:42 +0100

    Add docstrings

--- photon.py
+++ photon.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3.7
+"""Dead-simple tool for matching photos by name and hash."""
 from collections import defaultdict
 from functools import partial
 from hashlib import md5
@@ -37,6 +38,7 @@ def match(from_dir: str, to_dir: str):
 
 
 def _match(from_path: Path, to_path: Path) -> Iterable[Match]:
+    """Find duplicates in to_path relative to from_path."""
     from_map: PathGroup = _group_by(set(_scan_dir(from_path)))
     to_map: PathGroup = _group_by(set(_scan_dir(to_path)))
 
@@ -54,6 +56,7 @@ def _group_by(
         xs: Iterable[A],
         identify: Callable[[A], K] = attrgetter('name')
 ) -> Dict[K, List[A]]:
+    """Group xs by an identifier function."""
     res: Dict[K, List[A]] = defaultdict(list)
     for x in xs:
         res[identify(x)].append(x)
@@ -61,19 +64,23 @@ def _group_by(
 
 
 def _split(xs: Iterable[A], p: Callable[[A], bool]) -> Tuple[List[A], List[A]]:
+    """Split iterable by predicate into tuple of matching, non-matching."""
     grouped = _group_by(xs, p)
     return grouped.get(True, []), grouped.get(False, [])
 
 
 def _scan_dir(d: Path) -> Iterable[Path]:
+    """Generate all file paths under d."""
     return (f for f in d.rglob('*') if f.is_file() and not f.is_symlink())
 
 
 def _hash(file: Path) -> str:
+    """Compute the MD5 digest of a file."""
     return md5(file.read_bytes()).hexdigest()
 
 
 def _is_ancestor(d: Path, f: Path) -> bool:
+    """True if d is an ancestor of f."""
     return str(f.resolve()).startswith(str(d.resolve()))
 
 
